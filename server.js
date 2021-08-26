87db27ed14b435c256d3628fbd52cafd3ded9b93
juanraugutierrez <juan.gutierrezcalquin@gmail.com>
2021-08-26 06:02:21 -0400

    2
    prueba

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 var http= require("http");
+const { request } = require("node:http");
+var i=0;
 
 function iniciar() {
     function onRequest(request,response){
         console.log("peticion recibida.");
         response.writeHead(200,{"Content-Type": "text/html"});
         response.write("hola Mundo");
+        response.write(request,response);
         response.end();
     }
     http.createServer(onRequest).listen(8081);
     console.log("Servidor Iniciado");
+    i=i+1;
+    console.log(i);
+    console.log(request);
 }
 
 // http.createServer(function(request, response){
