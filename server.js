65aafc9ef458c2b61cf39902cc28ce35f03e80e9
juanraugutierrez <juan.gutierrezcalquin@gmail.com>
2021-08-29 19:57:05 -0400

    3
    funciona

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
 var http= require("http");
-const { request } = require("node:http");
-var i=0;
+
 
 function iniciar() {
     function onRequest(request,response){
         console.log("peticion recibida.");
         response.writeHead(200,{"Content-Type": "text/html"});
         response.write("hola Mundo");
-        response.write(request,response);
         response.end();
     }
-    http.createServer(onRequest).listen(8081);
-    console.log("Servidor Iniciado");
-    i=i+1;
-    console.log(i);
-    console.log(request);
-}
 
+    http.createServer(onRequest).listen(8081);
+    console.log("servidor iniciado");
+};
+exports.iniciar=iniciar;
 // http.createServer(function(request, response){
 //     response.writeHead(200,{"Content-Type": "text/html"});
 //     response.write("hola Mundo");
