ba6d31bffaea92eb8fc3273b4600f260382d39d2
juanraugutierrez <juan.gutierrezcalquin@gmail.com>
2021-08-29 21:23:00 -0400

    1
    nuevo

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http= require("http");
-
+var i=0;
 
 function iniciar() {
     function onRequest(request,response){
@@ -7,10 +7,15 @@ function iniciar() {
         response.writeHead(200,{"Content-Type": "text/html"});
         response.write("hola Mundo");
         response.end();
+        i=i+1;
+        console.log(i);
+      
     }
 
     http.createServer(onRequest).listen(8081);
     console.log("servidor iniciado");
+    i+=1;
+    
 };
 exports.iniciar=iniciar;
 // http.createServer(function(request, response){
